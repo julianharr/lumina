ddadc619706860a026fd7a5539ed3a7b16d91211
Julian Harrington <jrayharrington@gmail.com>
2021-02-23 20:33:12 +1100

    dashboard now shows wishlist and user avatar on page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,9 @@ class UsersController < ApplicationController
   # def user_params
   #   params.require(:user).permit(:name, :tag_list) ## Rails 4 strong params usage
   # end
+
+  def show
+    @user = User.find(params[:id])
+    @wishlist = Wishlist.where(user_id: @user.id)
+  end
 end
