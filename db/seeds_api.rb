1c168d5d1f6ad9a864237b98c69442d2ef7c4b91
Chris Wade <chris@wadespace.net>
2021-02-26 13:17:04 +1100

    working!!

--- db/seeds_api.rb
+++ db/seeds_api.rb
@@ -23,7 +23,7 @@ def get_initial_token
   response = https.request(request)
 
   result = JSON.parse(response.body)
-
+  p result&.key?("code") ? puts("--- Passed step one initial token") : puts("--- step 1 FAILED..")
   return result["code"] # {"code":"bc807ec3af945ed60fc1686c65c43344"}
 end
 
@@ -45,7 +45,7 @@ def request_access_token
 
   response = https.request(request)
   result = JSON.parse(response.body)
-
+  p result&.key?("access_token") ? puts("--- Passed step two access token") : puts("--- step 2 FAILED..")
   # {"access_token"=>"xxx", "refresh_token"=>"xxx", "member"=>{"country"=>"au", "city"=>"Melbourne", "name"=>"prereg_member_312483285115459", "lon"=>144.96689, "id"=>3272384992, "state"=>"07", "lat"=>-37.815903}, "token_type"=>"bearer", "expires_in"=>3600}
   return result
 end
@@ -69,20 +69,26 @@ def request_oauth_token
 
   response = https.request(request)
   result = JSON.parse(response.body)
+  p result&.key?("oauth_token") ? puts("--- Passed step three oauth_token") : puts("--- step e FAILED..")
+  return result
   # {"oauth_token"=>"xxx", "oauth_token_secret"=>"xxx", "refresh_token"=>"xxx", "expires_in"=>3600, "member"=>{"id"=>381717, "name"=>"someguy", "email"=>"email", "status"=>"active", "joined"=>1404391594000, "city"=>"Melbourne", "country"=>"au", "localized_country_name"=>"Australia", "lat"=>-37.81, "lon"=>144.96, "photo"=>{"id"=>303641096, "highres_link"=>"https://secure.meetupstatic.com/photos/member/a/0/8/8/highres_303641096.jpeg", "photo_link"=>"https://secure.meetupstatic.com/photos/member/a/0/8/8/member_303641096.jpeg", "thumb_link"=>"https://secure.meetupstatic.com/photos/member/a/0/8/8/thumb_303641096.jpeg", "type"=>"member", "base_url"=>"https://secure.meetupstatic.com"}, "is_pro_admin"=>true}}
 end
 
 # get the auth token
 
-def meetup_events_finder
-  cert_meetup_api
+def meetup_events_finder(options = {})
+  cert_meetup_api # oauth setup
+  # token from above
   bearer = request_oauth_token["oauth_token"]
-  # longitude
-  # latitude
-  # category_id
-  # radius
+  lon = options[:longitude]
+  lat = options[:latitude]
+  cat = options[:category] # text category field
+  # radius smart or kms
+  # order best or time
 
-  url = URI("https://api.meetup.com/find/upcoming_events?lon=145&page=20&radius=10&lat=-37.81")
+  p url = URI("https://api.meetup.com/find/upcoming_events?lon=#{lon}&page=20&radius=smart&lat=#{lat}&order=best&text=#{cat}&fields=featured_photo")
+  # original below
+  # p url = URI("https://api.meetup.com/find/upcoming_events?lon=145&page=20&radius=10&lat=#{lat}")
 
   https = Net::HTTP.new(url.host, url.port)
   https.use_ssl = true
@@ -92,13 +98,15 @@ def meetup_events_finder
   request["Cookie"] = "MEETUP_AFFIL=affil=meetup; MEETUP_BROWSER_ID=\"id=8f29c91f-3033-4eda-a298-190fc2071d55\"; MEETUP_CSRF=dbccdafc-8e61-4007-9a5a-4c92c81dbdc5; MEETUP_MEMBER=\"id=154567222&status=4&timestamp=1614293651&bs=0&tz=Australia%2FMelbourne&zip=meetup2&country=au&city=Melbourne&state=&lat=-37.81&lon=144.96&ql=false&s=802d872fc49dd2c821523298ccdec801cad92d53&scope=ALL\"; MEETUP_TRACK=id=5e0859a6-c334-4f2b-bd73-818934553be7&l=1&s=a7bf3696ab6fe266da7143de4cb39f9fc2698c45; SIFT_SESSION_ID=20670450-2624-4174-a947-cce0ab741e54"
 
   response = https.request(request)
-  pp result = JSON.parse(response.body)
+  pp result = JSON.parse(response.body) # result hash
 end
 
 def cert_meetup_api
-  get_initial_token # 1
-  request_access_token # 2
-  request_oauth_token # 3
+  # cascades all of above below not needed
+
+  # get_initial_token # 1
+  # request_access_token # 2
+  # request_oauth_token # 3
 end
 
-meetup_events_finder
+meetup_events_finder({ latitude: "-37.81", longitude: "144.96", category: "sex" })
