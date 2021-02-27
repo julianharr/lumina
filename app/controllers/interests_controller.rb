aae348d2722ef596190425d7bad3307c9583da57
Thomas Temple <tjt_84@hotmail.com>
2021-02-27 12:25:31 +1100

    interests set up complete

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -1,8 +1,14 @@
 class InterestsController < ApplicationController
 
   def create
+    @user = current_user
     @interest_array = params["interest"].split(",")
-    # current_user.interest.add(@interest)
+    @user.interest_list.add@interest_array
+    if @user.save
+      redirect_to feed_path
+    else
+      raise
+    end
   end
 
 end
