b4ff6ba40d253e0e91cb9186d80c883c9ef5cb0d
Julian Harrington <jrayharrington@gmail.com>
2021-03-02 11:21:08 +1100

    testing redis updates for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,4 +118,6 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.action_cable.allowed_request_origins = [
+  'https://love-lumina.herokuapp.com', 'http://love-lumina.herokuapp.com']
 end
