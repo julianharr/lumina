311390ecaf5b127db1325ca6bd042ca8a7a2fff1
Thomas Temple <tjt_84@hotmail.com>
2021-03-06 11:22:54 +1100

    added new sign up elements and fixed log out on side bar

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,4 +4,12 @@ class RegistrationsController < Devise::RegistrationsController
   def after_sign_up_path_for(resource)
     "/interests" # Or :prefix_to_your_route
   end
+
+
+private
+
+  def sign_up_params
+    params.require(:user).permit(:email, :password, :first_name, :last_name, :birthday, :avatar)
+  end
+
 end
