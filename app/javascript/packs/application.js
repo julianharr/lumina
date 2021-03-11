6f275830697af3bfb8959acf33cb4a4925c83853
Julian Harrington <jrayharrington@gmail.com>
2021-03-11 19:12:43 +1100

    turbolinks loads donate animation

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ import Rails from "@rails/ujs";
 import Turbolinks from "turbolinks";
 import * as ActiveStorage from "@rails/activestorage";
 import "channels";
-import "dom/donate";
+
 
 Rails.start();
 Turbolinks.start();
@@ -24,6 +24,7 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import { donateAnimation } from "dom/donate";
 
 // For Chatrooms
 import { initChatroomCable } from "../channels/chatroom_channel";
@@ -56,7 +57,7 @@ if (images) {
 // TURBO DOWN HERE !!!
 document.addEventListener("turbolinks:load", () => {
   initMapbox();
-  // createPost();
+  donateAnimation();
   initChatroomCable();
 });
 
